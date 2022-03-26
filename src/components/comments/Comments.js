f3edcdbd45ff74bcebd714e5aafd85cf4b2d4ebd
Marcus-Cox <marcus.cox92@gmail.com>
2022-03-26 10:04:37 -0500

    Light Comment and Regristartion rework

--- src/components/comments/Comments.js
+++ src/components/comments/Comments.js
@@ -1,4 +1,17 @@
-const Comments = () => {
+import {useState,useEffect} from "react";
+import {getComments as getCommentsApi} from "../api";
+
+const Comments = ({currentUserId}) => {
+    const [backendComments,setBackendComments] = useState([]);
+    
+    console.log("backendComments", backendComments);
+
+    useEffect(() => {
+        getCommentsApi().then((data) => {
+          setBackendComments(data);
+        });
+      }, []);
+
     return (
         <div>
             Comments
